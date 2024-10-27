78a82f3155b10eb39e400e38c990483849ebf356
Wallyson Alessandro Jeronimo Nabarro Lopes <wallyson.lopes.colaborador@incra.gov.br>
2024-10-27 14:58:51 -0300

    Cadastro de CPF cliente finalizado

--- FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
+++ FI.AtividadeEntrevista/DAL/Clientes/DaoCliente.cs
@@ -112,6 +112,7 @@ namespace FI.AtividadeEntrevista.DAL
             parametros.Add(new System.Data.SqlClient.SqlParameter("Sobrenome", cliente.Sobrenome));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Nacionalidade", cliente.Nacionalidade));
             parametros.Add(new System.Data.SqlClient.SqlParameter("CEP", cliente.CEP));
+            parametros.Add(new System.Data.SqlClient.SqlParameter("CPF", cliente.CPF));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Estado", cliente.Estado));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Cidade", cliente.Cidade));
             parametros.Add(new System.Data.SqlClient.SqlParameter("Logradouro", cliente.Logradouro));
